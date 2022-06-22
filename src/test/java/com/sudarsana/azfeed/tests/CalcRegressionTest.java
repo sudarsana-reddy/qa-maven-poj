7065c7ee19f09d4af996df3f54e7538bffc67ee2
Sud <sudarsana.ala@gmail.com>
2022-06-22 22:06:41 +0530

    created testng xml and another test class

--- src/test/java/com/sudarsana/azfeed/tests/CalcRegressionTest.java
+++ src/test/java/com/sudarsana/azfeed/tests/CalcRegressionTest.java
@@ -4,22 +4,22 @@ import com.sudarsana.azfeed.Calculator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CalcTest {
+public class CalcRegressionTest {
 
     @Test
-    public void testAdd(){
+    public void regressionTestAdd(){
         int result = new Calculator().add(2,3);
         Assert.assertEquals(result, 5, "Expected sum of 2 and 3 as 5");
     }
 
     @Test
-    public void testMultiply(){
+    public void regressionTestMultiply(){
         int result = new Calculator().multiply(2,3);
         Assert.assertEquals(result, 6, "Expected 2 * 3 as 6");
     }
 
     @Test
-    public void failTest(){
+    public void regressionFailTest(){
         Assert.assertEquals(4, 6, "Expected 2 * 3 as 6");
     }
 
---
+++ src/test/java/com/sudarsana/azfeed/tests/CalcSmokeTest.java
@@ -0,0 +1,26 @@
+package com.sudarsana.azfeed.tests;
+
+import com.sudarsana.azfeed.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalcSmokeTest {
+
+    @Test
+    public void smokeTestAdd(){
+        int result = new Calculator().add(2,3);
+        Assert.assertEquals(result, 5, "Expected sum of 2 and 3 as 5");
+    }
+
+    @Test
+    public void smokeTestMultiply(){
+        int result = new Calculator().multiply(2,3);
+        Assert.assertEquals(result, 6, "Expected 2 * 3 as 6");
+    }
+
+    @Test
+    public void smokeFailTest(){
+        Assert.assertEquals(4, 6, "Expected 2 * 3 as 6");
+    }
+
+}
