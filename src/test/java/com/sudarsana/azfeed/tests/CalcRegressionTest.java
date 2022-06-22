6be9ff678b424bab73739b88325218beeabee8f4
Sud <sudarsana.ala@gmail.com>
2022-06-22 22:08:32 +0530

    removed regression failing test

--- src/test/java/com/sudarsana/azfeed/tests/CalcRegressionTest.java
+++ src/test/java/com/sudarsana/azfeed/tests/CalcRegressionTest.java
@@ -7,20 +7,14 @@ import org.testng.annotations.Test;
 public class CalcRegressionTest {
 
     @Test
-    public void regressionTestAdd(){
-        int result = new Calculator().add(2,3);
+    public void regressionTestAdd() {
+        int result = new Calculator().add(2, 3);
         Assert.assertEquals(result, 5, "Expected sum of 2 and 3 as 5");
     }
 
     @Test
-    public void regressionTestMultiply(){
-        int result = new Calculator().multiply(2,3);
+    public void regressionTestMultiply() {
+        int result = new Calculator().multiply(2, 3);
         Assert.assertEquals(result, 6, "Expected 2 * 3 as 6");
     }
-
-    @Test
-    public void regressionFailTest(){
-        Assert.assertEquals(4, 6, "Expected 2 * 3 as 6");
-    }
-
 }
